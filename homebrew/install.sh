5573d29e4a4f9fa090fccd928f5fbf0176dc49b7
Amo Wu <amo260@gmail.com>
2015-01-22 04:16:45 +0800

    Add homebrew binaries, apps, fonts installation

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -4,6 +4,29 @@
 #
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
+binaries=(
+  git
+  mackup
+  tree
+)
+
+apps=(
+  alfred
+  dropbox
+  google-chrome
+  iterm2
+  qlcolorcode
+  qlmarkdown
+  qlstephen
+  sublime-text3
+)
+
+fonts=(
+  font-roboto
+  # font-noto-sans-t-chinese
+  # font-noto-sans
+  # font-noto-serif
+)
 
 # Check for Homebrew
 if test ! $(which brew)
@@ -12,7 +35,33 @@ then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
 fi
 
-# Install homebrew packages
-brew install grc coreutils spark
+# Update homebrew recipes
+brew update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+# Install Bash 4
+brew install bash
+# Install Homebrew Cask
+brew tap caskroom/fonts
+brew tap caskroom/versions
+brew install caskroom/cask/brew-cask
+
+echo "installing binaries..."
+brew install ${binaries[@]}
+
+echo "installing fonts..."
+# brew cask install ${fonts[@]}
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
+
+# clean things up
+brew cleanup
+brew cask cleanup
 
 exit 0
