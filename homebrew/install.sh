1cefa8351082717a67aa2900a8d0ea4b223db30c
Amo Wu <amo260@gmail.com>
2015-08-10 16:26:09 +0800

    Homebrew: close #7, ignore already installed error and warningsm.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -63,23 +63,23 @@ echo "Update Homebrew..."
 brew update
 
 # Install GNU core utilities (those that come with OS X are outdated)
-brew install coreutils
+brew install coreutils 2> /dev/null
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils
+brew install findutils 2> /dev/null
 # Install Bash 4
-brew install bash
+brew install bash 2> /dev/null
 # Install Homebrew Cask
-brew tap caskroom/fonts
-brew tap caskroom/versions
-brew install caskroom/cask/brew-cask
-brew upgrade brew-cask
+brew tap caskroom/fonts 2> /dev/null
+brew tap caskroom/versions 2> /dev/null
+brew install caskroom/cask/brew-cask 2> /dev/null
+brew upgrade brew-cask 2> /dev/null
 
 echo "Do you want to install or update Homebrew binaries?"
 select yn in "Yes" "No"; do
   case $yn in
     Yes )
       echo "Installing binaries...";
-      brew install ${binaries[@]};
+      brew install ${binaries[@]} 2> /dev/null;
       break;;
     No ) break;;
   esac
@@ -90,7 +90,7 @@ select yn in "Yes" "No"; do
   case $yn in
     Yes )
       echo "Installing fonts...";
-      brew cask install ${fonts[@]};
+      brew cask install ${fonts[@]} 2> /dev/null;
       break;;
     No ) break;;
   esac
@@ -103,7 +103,7 @@ select yn in "Yes" "No"; do
       # Install apps to /Applications
       # Default is: /Users/$user/Applications
       echo "Installing apps..."
-      sudo brew cask install --appdir="/Applications" ${apps[@]}
+      sudo brew cask install --appdir="/Applications" ${apps[@]} 2> /dev/null;
       break;;
     No ) break;;
   esac
