34c27b70dda5d0a074e42dda58ca6846db058ccc
李志威 <lizhiwei@elaebls.cn>
2017-10-27 17:45:24 +0800

    fix _path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:$HOME/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+export PATH="./bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:$ZSH/bin:$DOTFILES/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
